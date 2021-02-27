7d46f34ba3334197570a812f4b3d27b40a74f4bd
Paweł Zarzycki (PZAR) <pzar@demant.com>
2021-02-27 06:59:48 +0100

    logika gry przeniesiona do biblioteki

---
+++ Szachy/Treningi/Zbijak.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Szachy.Treningi
+{
+    public class Zbijak
+    {
+        public Szachownica szachownica = new Szachownica();
+        public IList<Bierka> atakiDoZnalezienia = new List<Bierka>();
+        public IList<Bierka> atakiZnalezione = new List<Bierka>();
+        public int bledy = 0;
+        public IList<Bierka> bierkiZaznaczone = new List<Bierka>();
+        public IList<string> fenRepository = new List<String>(); 
+        public Random random = new Random();
+
+        public Zbijak(string[] fens)
+        {
+            fenRepository = fens.ToList();
+        }
+
+        public Zbijak()
+        {
+        }
+
+        public void PokazBierkiZaatakoanePrzez(Bierka b)
+        {
+            bierkiZaznaczone.Clear();
+            foreach (var bierka in b.DajZaatakowaneBierki())
+            {
+                bierkiZaznaczone.Add(bierka);
+            }
+        }
+
+        public void SchowajBierkiZaatakowane()
+        {
+            bierkiZaznaczone.Clear();
+        }
+
+        public bool WskazanoBierke(Bierka b)
+        {
+            if (atakiDoZnalezienia.Except(atakiZnalezione).Contains(b))
+            {
+                atakiZnalezione.Add(b);
+                return true;
+                
+            }
+            else
+            {                
+                bledy++;
+                return false;
+            }
+        }
+
+        public void NastepnaPozycja()
+        {
+            do
+            {
+                szachownica.WgrajFEN(fenRepository[random.Next(0, fenRepository.Count())]);
+                atakiDoZnalezienia = szachownica.GetZaataKowaneBierki();
+            } while (atakiDoZnalezienia.Count() == 0);
+        }
+    }
+}
---
+++ Zbijak/Helpers/PiecesHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Szachy;
+
+namespace Zbijak.Helpers
+{
+    public class PiecesHelper
+    {
+        public string GetImageSrc(Bierka b)
+        {
+            var literaKoloru = b.Kolor == Kolor.Bialy ? "w" : "b";
+            var literaBierki =  DajLitereRodzajuBierki(b);
+            return $"sets/standard/{literaBierki}{literaKoloru}.png";
+        }
+
+        private static string DajLitereRodzajuBierki(Bierka b)
+        {
+            if (b is Krol) return "k";
+            if (b is Hetman) return "q";
+            if (b is Wieza) return "r";
+            if (b is Goniec) return "b";
+            if (b is Skoczek) return "n";
+            if (b is Pion) return "p";
+            throw new Exception("nie ma takiej bierki");
+        }       
+    }
+}
